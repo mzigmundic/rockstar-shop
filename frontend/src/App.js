717ec266c5044c91c694af70af4b0f1ef92696a2
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-07 17:06:58 +0200

    added place order page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import ShippingPage from "./pages/ShippingPage";
 import PaymentPage from "./pages/PaymentPage";
+import PlaceOrderPage from "./pages/PlaceOrderPage";
 
 function App() {
     return (
@@ -26,6 +27,7 @@ function App() {
                     <Route path="/profile" component={ProfilePage} />
                     <Route path="/shipping" component={ShippingPage} />
                     <Route path="/payment" component={PaymentPage} />
+                    <Route path="/place-order" component={PlaceOrderPage} />
                 </Container>
             </main>
             <Footer />
