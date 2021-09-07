77f253de7184d1a1a5cfca7ded343d6ca8824de8
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-07 15:26:07 +0200

    added payment page, saving payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import ShippingPage from "./pages/ShippingPage";
+import PaymentPage from "./pages/PaymentPage";
 
 function App() {
     return (
@@ -24,6 +25,7 @@ function App() {
                     <Route path="/register" component={RegisterPage} />
                     <Route path="/profile" component={ProfilePage} />
                     <Route path="/shipping" component={ShippingPage} />
+                    <Route path="/payment" component={PaymentPage} />
                 </Container>
             </main>
             <Footer />
