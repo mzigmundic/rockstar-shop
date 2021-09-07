abfff5ca74ce639340e3b905a369e92d35fb070d
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-07 13:21:24 +0200

    added functionality for updating user on profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -3,7 +3,7 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
 
 const ProfilePage = ({ location, history }) => {
     const [name, setName] = useState("");
@@ -13,11 +13,15 @@ const ProfilePage = ({ location, history }) => {
     const [message, setMessage] = useState(null);
 
     const dispatch = useDispatch();
+
     const userDetails = useSelector((state) => state.userDetails);
     const { loading, error, user } = userDetails;
 
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
+
+    const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+    const { success } = userUpdateProfile;
 
     useEffect(() => {
         if (!userInfo) {
@@ -37,7 +41,9 @@ const ProfilePage = ({ location, history }) => {
         if (password !== confirmPassword) {
             setMessage("Password do not match");
         } else {
-            // Dispatch update profile
+            dispatch(
+                updateUserProfile({ id: user._id, name, email, password })
+            );
         }
     };
 
@@ -47,6 +53,9 @@ const ProfilePage = ({ location, history }) => {
                 <h2>User Profile</h2>
                 {message && <Message variant="danger">{message}</Message>}
                 {error && <Message variant="danger">{error}</Message>}
+                {success && (
+                    <Message variant="success">Profile Updated</Message>
+                )}
                 {loading && <Loader />}
                 <Form onSubmit={submitHandler}>
                     <Form.Group controlId="name">
