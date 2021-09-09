edb4fc29c129cb83b85a57d4bf0fb8088c9f70e4
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-09 13:51:42 +0200

    create & update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,6 +23,52 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc    Create a Product
+// @route   POST /api/products/:id
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: "Sample name",
+        price: 0,
+        user: req.user._id,
+        image: "/imgs/sample.jpg",
+        brand: "Sample brand",
+        category: "Sample category",
+        countInStock: 0,
+        numReviews: 0,
+        description: "Sample description",
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+});
+
+// @desc    Update a Product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, image, brand, category, countInStock, description } =
+        req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        product.name = name;
+        product.price = price;
+        product.image = image;
+        product.brand = brand;
+        (product.category = category),
+            (product.countInStock = countInStock),
+            (product.description = description);
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error("Product not found");
+    }
+});
+
 // @desc    Delete a Product
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
@@ -38,4 +84,10 @@ const deleteProduct = asyncHandler(async (req, res) => {
     }
 });
 
-export { getProducts, getProductById, deleteProduct };
+export {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+};
