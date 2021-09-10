24f657edf84b764ed24fb5784b94836e58ac047c
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-10 11:19:09 +0200

    added morgan for logging when hitting routes, created reviews endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
     deleteProduct,
     updateProduct,
     createProduct,
+    createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -16,5 +17,6 @@ router
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
