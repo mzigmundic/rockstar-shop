53b024ddd28008895c6ac20f5a2c6d320b890c9d
mzigmundic <mladen.zigmundic@gmail.com>
2021-09-12 21:20:44 +0200

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,12 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
-app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -34,6 +31,17 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
